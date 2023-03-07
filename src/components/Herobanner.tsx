4319a7d4811cf2ee06ccfcd26d6a2088800a9e5f
Md. Arshad <arshadchowdhury46@gmail.com>
2023-03-07 14:14:34 +0600

    almost done with homepage need to make other pages

--- src/components/Herobanner.tsx
+++ src/components/Herobanner.tsx
@@ -1,27 +1,33 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Navbar from "./Navbar";
 
 const Herobanner = () => {
   return (
     <>
-    <div>
-        <div>
-            <p>Small Text</p>
-            <h3>Mid Text</h3>
-            <Image src="/Arshad_pic.JPG" height="300" width="300" alt="Hero-Banner-Image"/>
-            <div>
-                <Link href="/product/id">
-                    <button type='button'>Button Text</button>
-                </Link>
-                <div>
-                    <h5>Desc</h5>
-                     <p>Description</p></div>
-            </div>
+      <Navbar />
+      <div className="container h-10 mx-auto p-5">
+        <div className="flex mt-32">
+          <div className="flex flex-col justify-start w-2/5 mt-24">
+            <h2 className="font-serif text-5xl text-gray-600 mb-4">Some Catchy Title Here</h2>
+            <p className="text-gray-600 uppercase tracking-widest mb-1">Our Brand Tagline Goes Here</p>
+            <p className="text-gray-600 uppercase tracking-widest">Our Brand Motto Goes Here</p>
+            <button className="bg-gradient-to-r from-red-600 to-pink-600 rounded-full py-4 px-8 text-gray-50 uppercase text-xl self-start my-4">Shop Now</button>
+          </div>
+          <div className="w-3/5">
+            <Image
+              src="/hero-img.svg"
+              height="300"
+              width="300"
+              alt="Arshad"
+              className="w-full"
+              />
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Herobanner
+export default Herobanner;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <div className="container h-10 mx-auto p-5">
+        <div className="flex justify-between">
+          <div className="flex cursor-pointer">
+            <div className="bg-gradient-to-r from-purple-400 to-red-600 w-10 h-10 rounded-lg"></div>
+            <h1 className="text-3xl text-gray-600 ml-2">Logo</h1>
+          </div>
+          <div>
+            <Link href="/" className="text-gray-600 hover:text-purple-600 p-4">
+              Home
+            </Link>
+            <Link
+              href="/shop"
+              className="text-gray-600 hover:text-purple-600 p-4"
+            >
+              Shop
+            </Link>
+            <Link
+              href="/blog"
+              className="text-gray-600 hover:text-purple-600 p-4"
+            >
+              Blog
+            </Link>
+            <Link
+              href="/products"
+              className="text-gray-600 hover:text-purple-600 p-4"
+            >
+              Products
+            </Link>
+            <Link
+              href="/contact"
+              className="text-gray-600 hover:text-purple-600 p-4"
+            >
+              Contact
+            </Link>
+            <Link
+              href="/contact"
+              className="bg-purple-600 text-gray-50 rounded-lg hover:bg-purple-900 p-3 px-5"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6 mr-2 inline-block"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                />
+              </svg>
+              Cart (0)
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
